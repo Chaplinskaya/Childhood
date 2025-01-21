4e606bf9e593054019171edf67e431b376d63946
Tetiana <chaplinskaya.tatyana@gmail.com>
2025-01-21 21:09:47 +0100

    the end my project wordpress

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+
+get_header();
+?>
+
+<div class="container toys">
+	<h2 class="subtitle">
+		<a href="<?php echo get_home_url(); ?>">
+			This page doesn't found. Return to Home?
+		</a>
+	</h2>
+</div>
+	
+<?php
+get_footer();
+?>
